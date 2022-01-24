c1aec1d82f6fe5aa455af17807a5ea1eba650987
Vladimir Fibe <motiw@icloud.com>
2022-01-24 07:21:18 +0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vladimir Fibe on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
